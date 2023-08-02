c0884edbf73314a4623e9312458e2fe8230a80f0
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-08-02 11:37:33 +0300

    added search history functionality

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,17 +1,30 @@
-import React,{useContext} from 'react'
+import React,{useContext,useEffect,useState} from 'react'
 import { CartContext } from './CartProvider'
 import ProductItem from './productItem'
 import ProductDetails from './ProductDetails'
 
 const Cart=()=>{
     const {cart}=useContext(CartContext)
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/products")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            setProducts(data);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      }, []);
+    
 
     return(
         <div>
             <h1 className="text-rose-600">SEARCH HISTORY</h1>
              <ul>
                 {cart.map((product,index)=>(
-                   <ProductItem/>
+                   <ProductItem key={product.id} {...product} />
                 ))}
              </ul>
         </div>
