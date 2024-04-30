ac9dce19b82301572960d3dcddbf3f309049d2dd
JessyWaweru <126556708+JessyWaweru@users.noreply.github.com>
2024-04-30 17:18:42 +0300

    Update updateProduct.js
    some comments for understanding

--- client/src/components/updateProduct.js
+++ client/src/components/updateProduct.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
+//useState allows for managing and updating state
 import { useNavigate, useParams } from "react-router-dom";
+//useNavigate hook is used to navigate to different routes in your app.
+//useParams provides a simple and convenient way to access URL parameters within your components
 import ProductForm from "./ProductForm";
 
 export default function UpdateProduct() {
   const [errorMsg, setErrorMsg] = useState("");
   const { id } = useParams();
+  //when you call useParams(),it returns an object containing the parameters parsed from the URL,hence the {}
   const navigate = useNavigate();
   const [product, setProduct] = useState(null);
 
@@ -18,7 +22,9 @@ export default function UpdateProduct() {
     };
     getProduct();
   }, [id]);
-
+//useEffect hook is used to perform side effects in your functional components,such as data fetching
+  //the second argument inside the parentheses is an optional dependency array;when you provide it ,React will rerun the effect whenever any of the dependencies change.If the dependency array is empty,the effect will only run once after the initial render.If you omit the dependency array,the effeect will run after every render
+  
   const handleUpdateProduct = async (productDetails) => {
     try {
       const response = await fetch(
@@ -47,6 +53,8 @@ export default function UpdateProduct() {
   return (
     <>
       {product && (
+        //the product && part means that the following jsx will only be rendered iff product is truthy,ie not null,udefined,false,epty,or zero
+        
         <ProductForm
           isUpdatePage={true}
           productData={product}
